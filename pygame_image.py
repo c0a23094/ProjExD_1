874df05ecdc1c89094db87aae86ab2b9ca0bd8e0
c0a23094 <c0a2309438@edu.teu.ac.jp>
2025-04-15 16:53:31 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -10,32 +10,33 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img2 = pg.transform.flip(bg_img, True, False)
+    # bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_rct = kk_img.get_rect()
-    kk_rct.center = 300, 200
+    # kk_rct = kk_img.get_rect()
+    # kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
             
-            key_list = pg.key.get_pressed() # 操作
-            if key_list[pg.K_UP]:
-                kk_rct.move_ip((0, -1))
-                # print("key_list[pg.K_UP]")
-            if key_list[pg.K_DOWN]:
-                kk_rct.move_ip((0, +1))
-            if key_list[pg.K_LEFT]:
-                kk_rct.move_ip((-1, 0))
-            if key_list[pg.K_RIGHT]:
-                kk_rct.move_ip((+1, 0))
+            # key_list = pg.key.get_pressed() # 操作
+            # if key_list[pg.K_UP]:
+            #     kk_rct.move_ip((0, -1))
+            #     # print("key_list[pg.K_UP]")
+            # if key_list[pg.K_DOWN]:
+            #     kk_rct.move_ip((0, +1))
+            # if key_list[pg.K_LEFT]:
+            #     kk_rct.move_ip((-1, 0))
+            # if key_list[pg.K_RIGHT]:
+            #     kk_rct.move_ip((+1, 0))
 
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img2, [-x+1600, 0])
-        screen.blit(bg_img, [-x+3200, 0])      
-        screen.blit(kk_img, kk_rct)
+        # screen.blit(bg_img2, [-x+1600, 0])
+        # screen.blit(bg_img, [-x+3200, 0])      
+        # screen.blit(kk_img, kk_rct)
+        screen.blit(kk_img, [300, 200])  # 練習４
         pg.display.update()
         tmr += 1 
 

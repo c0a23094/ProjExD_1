a7e02b7e5bedfaf65f50ba808d38f1be1736409e
c0a23094 <c0a2309438@edu.teu.ac.jp>
2025-04-15 15:49:15 +0900

    練習2

--- pygame_image.py
+++ pygame_image.py
@@ -17,7 +17,6 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(100)
